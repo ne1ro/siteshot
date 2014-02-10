c7ece3630066a2787c9d37cc5f1a5ce0521a1272
neiro <neiro.mail@gmail.com>
2014-02-10 18:34:10 +0400

    Generate config

--- app/siteshot.coffee
+++ app/siteshot.coffee
@@ -1,5 +1,23 @@
 class SiteShot
   constructor: ->
+    @fs = require 'fs'
+
+    # Check for config param in arguments
+    if process.argv.indexOf 'config' != -1
+      @config()
+    else
+      @config = require 'siteshot.json'
+
+  # Generate config file
+  config: ->
+    example =
+      snapshotDir: 'snapshots'
+      sitemap: 'sitemap.xml'
+      opts:
+        cutImg: yes
+        cutJs: yes
+        cutCss: yes
+    @fs.writeFileSync 'siteshot.json', JSON.stringify example
 
 module.exports = SiteShot
 new SiteShot
--- dist/siteshot.js
+++ dist/siteshot.js
@@ -1,7 +1,28 @@
 var SiteShot;
 
 SiteShot = (function() {
-  function SiteShot() {}
+  function SiteShot() {
+    this.fs = require('fs');
+    if (process.argv.indexOf('config' !== -1)) {
+      this.config();
+    } else {
+      this.config = require('siteshot.json');
+    }
+  }
+
+  SiteShot.prototype.config = function() {
+    var example;
+    example = {
+      snapshotDir: 'snapshots',
+      sitemap: 'sitemap.xml',
+      opts: {
+        cutImg: true,
+        cutJs: true,
+        cutCss: true
+      }
+    };
+    return this.fs.writeFileSync('siteshot.json', JSON.stringify(example));
+  };
 
   return SiteShot;
 
