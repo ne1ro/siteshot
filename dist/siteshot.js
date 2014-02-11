fecb36d131b57b4b0aade288efb5cfec6791b9f6
neiro <neiro.mail@gmail.com>
2014-02-11 12:40:06 +0400

    Update js dist

--- dist/siteshot.js
+++ dist/siteshot.js
@@ -2,7 +2,7 @@ var SiteShot;
 
 SiteShot = (function() {
   function SiteShot() {
-    var parseString, phantom, _;
+    var async, mkdirp, parseString, path, phantom, url, _;
     this.fs = require('fs');
     if (process.argv.indexOf('config') !== -1) {
       this.config();
@@ -11,23 +11,54 @@ SiteShot = (function() {
       parseString = require('xml2js').parseString;
       _ = require('underscore');
       phantom = require('phantom');
-      parseString(this.fs.readFileSync(this.config.sitemap), function(err, result) {
-        var routes;
-        if (err != null) {
-          throw err;
-        } else {
-          routes = _.flatten(_.pluck(result.urlset.url, 'loc'));
-          return _.each(routes, function(route) {
+      async = require('async');
+      url = require('url');
+      path = require('path');
+      mkdirp = require('mkdirp');
+      parseString(this.fs.readFileSync(this.config.sitemap), (function(_this) {
+        return function(err, result) {
+          var routes;
+          if (err != null) {
+            throw err;
+          } else {
+            routes = _.flatten(_.pluck(result.urlset.url, 'loc'));
             return phantom.create(function(ph) {
-              return ph.createPage(function(page) {
-                return page.open(route, function(status) {
-                  return console.log(status);
+              var items;
+              items = [];
+              return _.each(routes, function(route, i) {
+                return ph.createPage(function(page) {
+                  return page.open(route, function(status) {
+                    if (status === 'success') {
+                      return page.evaluate((function() {
+                        return document.documentElement.outerHTML;
+                      }), function(res) {
+                        var snapPath, snapPrefix;
+                        snapPrefix = url.parse(route).path === '/' ? '/index' : url.parse(route).path;
+                        snapPath = "" + _this.config.snapshotDir + snapPrefix + ".html";
+                        return mkdirp(path.dirname(snapPath), function(err) {
+                          if (err != null) {
+                            throw err;
+                          }
+                          return _this.fs.writeFile(snapPath, res, function(err) {
+                            if (err != null) {
+                              throw err;
+                            }
+                            items.push(i);
+                            page.close();
+                            if (items.length === routes.length) {
+                              return ph.exit();
+                            }
+                          });
+                        });
+                      });
+                    }
+                  });
                 });
               });
             });
-          });
-        }
-      });
+          }
+        };
+      })(this));
     }
   }
 
