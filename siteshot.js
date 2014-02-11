c1ce8ac4db0ffca481df5882a767475b258b1f8c
neiro <neiro.mail@gmail.com>
2014-02-11 14:58:15 +0400

    Get locations list

--- siteshot.js
+++ siteshot.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Require modules and libs
-var xml2js = require('xml2js'),
+var parseXML = require('xml2js').parseString,
   fs = require('fs'),
   _ = require('lodash'),
   phantom = require('phantom'),
@@ -17,9 +17,17 @@ if (process.argv.indexOf('config') != -1) {
     sitemap: process.cwd() + '/sitemap.xml'
   };
   fs.writeFileSync(process.cwd() + '/siteshot.json', JSON.stringify(example));
-
 }
 else {
-  config = require(process.cwd() + '/siteshot.json');
+  var config = require(process.cwd() + '/siteshot.json');
+
+  // Read sitemap file and parse XML
+  parseXML(fs.readFileSync(config.sitemap), function(err, sitemap) {
+    if (err) { throw(err);}
+
+    // Get locations list from sitemap
+    routes = _.flatten(_.pluck(sitemap.urlset.url, 'loc'));
+    console.log(routes);
+  });
 }
 
